c38f2a13661c10ea756c0de443cdde46989ebf06
root <root@jenkinstest.01b1wljsxgte5goturtbw2obgg.parx.internal.cloudapp.net>
2025-02-11 14:38:50 +0000

    new

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 import * as api from './api.js';
 
-const host = 'http://localhost:3030'
-api.settings.host = 'http://localhost:3030';
+const host = '98.66.170.165:8081'
+api.settings.host = '98.66.170.165:8081'';
 
 export const login = api.login;
 export const register = api.register;
